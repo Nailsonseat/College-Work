89b14773f7bb114e739ac093b1f07d26faeb3ce4
nailsonseat <aadarshv3@gmail.com>
2023-02-01 15:49:37 +0530

    delete function for all three cases

--- C++_DSA/BinarySearchTree_DSA.cpp
+++ C++_DSA/BinarySearchTree_DSA.cpp
@@ -36,6 +36,15 @@ class BST
             traverse(item, parent, child, toDelete);
         }
     }
+    int countChild(Node const *node) const
+    {
+        if (node->left != nullptr && node->right != nullptr)
+            return 2;
+        else if (node->left == nullptr && node->right == nullptr)
+            return 0;
+        else
+            return 1;
+    }
 
 public:
     void getInOrder(Node *node)
@@ -72,6 +81,66 @@ public:
         }
         getInOrder(root);
     }
+    void deleteNode(int item)
+    {
+        Node *parent(root);
+        Node *node(nullptr);
+        if (parent->data == item)
+        {
+            node = parent;
+            parent = nullptr;
+        }
+        traverse(item, parent, node, true);
+        if (node == nullptr)
+            return;
+        if (countChild(node) == 0)
+        {
+            if (parent != nullptr)
+            {
+                if (parent->left != nullptr && parent->left->data == node->data)
+                    parent->left = nullptr;
+                else if (parent->right != nullptr && parent->right->data == node->data)
+                    parent->right = nullptr;
+            }
+            delete node;
+        }
+        else if (countChild(node) == 1)
+        {
+            if (node->left != nullptr)
+            {
+                node->data = node->left->data;
+                delete node->left;
+                node->left = nullptr;
+            }
+            else
+            {
+                node->data = node->right->data;
+                delete node->right;
+                node->right = nullptr;
+            }
+        }
+        else
+        {
+            int inOrderSucessor(0);
+            for (int i(0); i < inOrder.size(); i++)
+                if (inOrder[i] == item)
+                    inOrderSucessor = inOrder[i + 1];
+            Node *inOrderSucessorNode(nullptr);
+            traverse(inOrderSucessor, parent, inOrderSucessorNode, true);
+            if (parent->left->data == inOrderSucessorNode->data)
+            {
+                parent->left = nullptr;
+                node->data = inOrderSucessor;
+                delete inOrderSucessorNode;
+            }
+            else
+            {
+                parent->right = nullptr;
+                node->data = inOrderSucessor;
+                delete inOrderSucessorNode;
+            }
+        }
+    }
 };
 
 int main()
@@ -83,5 +152,6 @@ int main()
     b.insert(1);
     b.insert(2);
     b.insert(5);
+    b.deleteNode(4);
     return 0;
 }
